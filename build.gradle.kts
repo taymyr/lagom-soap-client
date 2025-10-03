3c1dcba1c41e450441ccbb11da9062c22265dd06
Sergey Morgunov <hostage@me.com>
2025-10-03 16:47:06 +0300

    Update CI deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import fr.brouillard.oss.jgitver.Strategies.MAVEN
 import java.time.Duration
 
 plugins {
-    id("io.github.gradle-nexus.publish-plugin") version "1.0.0"
+    id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
     id("fr.brouillard.oss.gradle.jgitver") version "0.9.1"
 }
 
@@ -21,7 +21,11 @@ nexusPublishing {
     packageGroup.set("org.taymyr")
     clientTimeout.set(Duration.ofMinutes(60))
     repositories {
-        sonatype()
+        // see https://central.sonatype.org/publish/publish-portal-ossrh-staging-api/#configuration
+        sonatype {
+            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
+        }
     }
 }
 
